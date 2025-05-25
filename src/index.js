4b37bc7e6f9c274ec52f0cc858aeb35aea7a0d95
Inkersal <inkersal501@gmail.com>
2025-05-25 21:32:55 +0530

    cors error clear1

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 app.use(express.json());
 app.use(compression());
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(passport.initialize());
 passport.use("jwt", jwtStrategy);
 
