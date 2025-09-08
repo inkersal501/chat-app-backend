aba3035c1af80926f58dcd698818d05c2dbc2922
Inkersal <inkersal501@gmail.com>
2025-09-08 21:22:58 +0530

    updated redis connection type

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,10 +1,10 @@
 import {createClient} from "redis";
 import config from "./config/config.js";
 
-const client = createClient();
+const client = createClient({url: config.redisURL, socket: {tls: true}});
 
 client.on("error", (error) => console.error("Redis Error:", error));
 
-await client.connect({url: config.redisURL, socket: {tls: true}});
+await client.connect();
 console.log("Redis Connected");
 export default client;
